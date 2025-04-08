6c1825114007a35ccbac7579890c5f351ed65465
Patrick Wang <patrickkwang@users.noreply.github.com>
2025-04-07 21:42:01 -0400

    Generate Outlook message
    Also trim return from each line.

--- generate_reports.sh
+++ generate_reports.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e
 
-while IFS=, read -r email name username
+while IFS=, read -r email name last
 do
+    username=${last%$'\r'}
     echo $username
     ./project_status.py $username
     pdflatex -output-directory outputs outputs/$username.tex
     echo "outputs/$username.pdf"
+    osascript sendMail.scpt $username $email $name
 done  < <(tail -n +2 $1)
