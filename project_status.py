566c823ffa66833730cd52a8c3ab779668b71b9a
Patrick Wang <patrickkwang@users.noreply.github.com>
2025-05-03 14:23:50 -0400

    Separate more document-generation stuff

--- project_status.py
+++ project_status.py
@@ -17,6 +17,8 @@ from zoneinfo import ZoneInfo
 import httpx
 from pr_state_machine import PrState, PrStateMachine, ReviewerState
 
+NUM_PHASES = 6
+
 
 def guess_phase(pr_title: str) -> int | None:
     """Guess what phase a PR is associated with, based on the title."""
@@ -199,6 +201,110 @@ def get_phase_mapping_overrides(filename: str) -> dict[str, dict[int, list[int]]
     return phase_mapping_overrides
 
 
+def write_document(username: str, summaries):
+    def create_page_header(phase: int, pr: PullRequest) -> str:
+        return (
+            f"\\fancyfoot[R]{{\\setfont phase {phase:02}}}"
+            + "\n\\noindent\n\\textbf{pull request}:\\\\\n"
+            + f'"{escape_latex(pr.title)}" (branch "{pr.branch}")\\\\\n'
+            + f"\\url{{{pr.permalink}}}\\\\\n"
+            + (
+                "\\\\\n"
+                + "\\textbf{inferred phase}:\\\\\n"
+                + f"{phase:02} (\\url{{https://github.com/biostat821/ehr-utils-project/blob/main/phase{phase:02}.md}})\\\\\n"
+                if phase in set(range(1, NUM_PHASES + 1))
+                else ""
+            )
+        )
+
+    pages = [
+        create_page_header(phase, pr) + summary for phase, pr, summary in summaries
+    ]
+    document = textwrap.dedent(f"""
+                \\documentclass{{article}}
+                \\usepackage[includehead, includefoot, portrait, margin=0.5in]{{geometry}}
+                \\usepackage{{booktabs}}
+                \\usepackage[colorlinks=true, urlcolor=blue]{{hyperref}}
+                \\usepackage{{longtable}}
+                \\usepackage{{fancyhdr}}               
+                \\usepackage{{lmodern}}
+                \\usepackage[normalem]{{ulem}}
+                \\newcommand{{\\setfont}}{{
+                    \\ttfamily\\fontseries{{l}}\\selectfont\\small
+                }}
+                \\begin{{document}}
+                \\pagestyle{{fancy}}
+                \\fancyhead{{}} \\fancyfoot{{}}
+                \\fancyhead[L]{{\\setfont {now().strftime("%Y-%m-%d %H:%M:%S")}}}
+                \\fancyhead[C]{{\\setfont {username}}}
+                \\fancyhead[R]{{\\setfont \\href{{https://github.com/biostat821/ehr-utils-project-status/tree/v1.0.0}}{{ehr-utils-project-status 1.0.0}}}}
+                \\ttfamily
+                \\fontseries{{l}}\\selectfont
+                \\small""").strip()
+    document += "\n\\pagebreak\n".join(pages)
+    document += "\n\\end{document}"
+    document = document.replace("_", "\\_")
+    with open(f"outputs/{username}.tex", "w") as f:
+        f.write(document)
+
+
+def construct_document(
+    due_date: datetime | None,
+    original_due_date: datetime,
+    finish_time: datetime | None,
+    extension: Extension | None,
+    entries,
+    total_under_development_duration: timedelta,
+    total_under_review_duration: timedelta,
+    out_of_slo_duration: timedelta,
+    late_by: timedelta | None,
+    adjusted_lateness: timedelta,
+    cumulative_adjusted_lateness: timedelta,
+    points_deducted: int | None,
+) -> str:
+    document = ""
+    if due_date:
+        document += "approval due"
+        if due_date != original_due_date:
+            document += f" \\sout{{{original_due_date.strftime('%Y-%m-%d %H:%M:%S')}}}"
+        document += f" {due_date.strftime('%Y-%m-%d %H:%M:%S')}"
+        if extension:
+            document += " (extension granted)\\\\\n"
+    document += textwrap.dedent("""
+                                \\setlength\\LTleft{0pt}
+                                \\setlength\\LTright{0pt}
+                                \\begin{longtable}{@{\\extracolsep{\\fill}}llr}
+                                \\toprule
+                                \\textbf{timestamp} & \\textbf{event} & \\textbf{status} \\\\
+                                \\midrule
+                                """).strip()
+    for event_summary, previous_state, elapsed_in_state in entries:
+        if elapsed_in_state:
+            document += f"{event_summary} & {previous_state.value} for {pad_to(td_to_str(elapsed_in_state), 17)} \\\\\n"
+        else:
+            document += f"{event_summary} & \\\\\n"
+    document += "\midrule\n"
+    document += f"&& under development for {pad_to(td_to_str(total_under_development_duration), 17)} \\\\\n"
+    document += f"&& under review for {pad_to(td_to_str(total_under_review_duration), 17)} \\\\\n"
+    document += (
+        f"&& reviews out of SLO for {pad_to(td_to_str(out_of_slo_duration), 17)} \\\\\n"
+    )
+    if late_by:
+        document += f"&& late by {pad_to(td_to_str(late_by), 17)} \\\\\n"
+    document += (
+        f"&& adjusted lateness: {pad_to(td_to_str(adjusted_lateness), 17)} \\\\\n"
+    )
+    document += "\midrule\n"
+    document += f"&& cumulative adjusted lateness: {pad_to(td_to_str(cumulative_adjusted_lateness), 17)} \\\\\n"
+    if points_deducted is not None:
+        document += f"&& \\textbf{{points deducted}}: \\textbf{{{pad_to(points_deducted, 17)}}} \\\\\n"
+    document += textwrap.dedent("""
+                                \\bottomrule
+                                \end{longtable}
+                                """).strip()
+    return document
+
+
 class EhrProjectStatus:
     """Multitool for managing EHR projects on GitHub."""
 
@@ -253,38 +359,21 @@ class EhrProjectStatus:
         last_approval: datetime | None = None,
         prior_adjusted_lateness: timedelta = timedelta(0),
     ) -> tuple[str, datetime | None, timedelta]:
-        document = ""
+        due_date = None
         if phase is not None:
             extension = self.extensions.get((pr.owner, phase))
             rolling_due_date = (
                 last_approval + self.phase_durations[phase]
-                if last_approval and phase <= 6
+                if last_approval and phase <= NUM_PHASES
                 else self.first_due_date
             )
             original_due_date = self.merge_due_dates[phase - 1]
             scheduled_due_date = extension.due_date if extension else original_due_date
-            if rolling_due_date:
-                due_date = min(
-                    max(rolling_due_date, scheduled_due_date), self.final_due_date
-                )
-            else:
-                due_date = scheduled_due_date
-            document += "approval due"
-            if due_date != original_due_date:
-                document += (
-                    f" \\sout{{{original_due_date.strftime('%Y-%m-%d %H:%M:%S')}}}"
-                )
-            document += f" {due_date.strftime('%Y-%m-%d %H:%M:%S')}"
-            if extension:
-                document += " (extension granted)\\\\\n"
-        document += textwrap.dedent("""
-                                    \\setlength\\LTleft{0pt}
-                                    \\setlength\\LTright{0pt}
-                                    \\begin{longtable}{@{\\extracolsep{\\fill}}llr}
-                                    \\toprule
-                                    \\textbf{timestamp} & \\textbf{event} & \\textbf{status} \\\\
-                                    \\midrule
-                                    """).strip()
+            due_date = (
+                min(max(rolling_due_date, scheduled_due_date), self.final_due_date)
+                if rolling_due_date
+                else scheduled_due_date
+            )
         all_events = sorted(
             [Created(pr.created_at)]
             + pr.timeline_events
@@ -360,42 +449,35 @@ class EhrProjectStatus:
             late_by = max(pr_state_machine.finish_time - due_date, timedelta(0))
             adjusted_lateness = max(late_by - out_of_slo, timedelta(0))
         else:
+            late_by = None
             adjusted_lateness = -out_of_slo
         cumulative_adjusted_lateness = adjusted_lateness + prior_adjusted_lateness
         if pr_state_machine.finish_time:
             points_deducted = max(
                 math.ceil(cumulative_adjusted_lateness / timedelta(days=1)), 0
             )
+        else:
+            points_deducted = None
 
-        # construct document
-        for event_summary, previous_state, elapsed_in_state in entries:
-            if elapsed_in_state:
-                document += f"{event_summary} & {previous_state.value} for {pad_to(td_to_str(elapsed_in_state), 17)} \\\\\n"
-            else:
-                document += f"{event_summary} & \\\\\n"
-        document += "\midrule\n"
-        document += f"&& under development for {pad_to(td_to_str(pr_state_machine.total_under_development_duration), 17)} \\\\\n"
-        document += f"&& under review for {pad_to(td_to_str(pr_state_machine.total_under_review_duration), 17)} \\\\\n"
-        document += (
-            f"&& reviews out of SLO for {pad_to(td_to_str(out_of_slo), 17)} \\\\\n"
+        document = construct_document(
+            due_date,
+            original_due_date,
+            pr_state_machine.finish_time,
+            extension,
+            entries,
+            pr_state_machine.total_under_development_duration,
+            pr_state_machine.total_under_review_duration,
+            out_of_slo,
+            late_by,
+            adjusted_lateness,
+            cumulative_adjusted_lateness,
+            points_deducted,
         )
-        if due_date and pr_state_machine.finish_time:
-            document += f"&& late by {pad_to(td_to_str(late_by), 17)} \\\\\n"
-        document += (
-            f"&& adjusted lateness: {pad_to(td_to_str(adjusted_lateness), 17)} \\\\\n"
-        )
-        document += "\midrule\n"
-        document += f"&& cumulative adjusted lateness: {pad_to(td_to_str(cumulative_adjusted_lateness), 17)} \\\\\n"
         if pr_state_machine.finish_time:
-            document += f"&& \\textbf{{points deducted}}: \\textbf{{{pad_to(points_deducted, 17)}}} \\\\\n"
             with open("outputs/_summary.csv", "a") as f:
                 f.write(
                     f'"{self.name}",{self.username},{phase},{pr.permalink},{100 - points_deducted}\n'
                 )
-        document += textwrap.dedent("""
-                                    \\bottomrule
-                                    \end{longtable}
-                                    """).strip()
         return document, approval, adjusted_lateness
 
     def infer_phases(self, pr: PullRequest, idx: int) -> list[int]:
@@ -455,55 +537,12 @@ class EhrProjectStatus:
                 )
                 cumulative_adjusted_lateness += adjusted_lateness
                 summaries.append((phase, pr, summary))
-            if approval and phase < 6:
+            if approval and phase < NUM_PHASES:
                 last_approval = approval
             else:
                 last_approval = None
 
-        def create_page(phase: int, pr: PullRequest, summary: str) -> str:
-            return (
-                f"\\fancyfoot[R]{{\\setfont phase {phase:02}}}"
-                + "\n\\noindent\n\\textbf{pull request}:\\\\\n"
-                + f'"{escape_latex(pr.title)}" (branch "{pr.branch}")\\\\\n'
-                + f"\\url{{{pr.permalink}}}\\\\\n"
-                + (
-                    "\\\\\n"
-                    + "\\textbf{inferred phase}:\\\\\n"
-                    + f"{phase:02} (\\url{{https://github.com/biostat821/ehr-utils-project/blob/main/phase{phase:02}.md}})\\\\\n"
-                    if phase in {1, 2, 3, 4, 5, 6}
-                    else ""
-                )
-                + summary
-            )
-
-        pages = [create_page(phase, pr, summary) for phase, pr, summary in summaries]
-
-        document = textwrap.dedent(f"""
-                    \\documentclass{{article}}
-                    \\usepackage[includehead, includefoot, portrait, margin=0.5in]{{geometry}}
-                    \\usepackage{{booktabs}}
-                    \\usepackage[colorlinks=true, urlcolor=blue]{{hyperref}}
-                    \\usepackage{{longtable}}
-                    \\usepackage{{fancyhdr}}               
-                    \\usepackage{{lmodern}}
-                    \\usepackage[normalem]{{ulem}}
-                    \\newcommand{{\\setfont}}{{
-                        \\ttfamily\\fontseries{{l}}\\selectfont\\small
-                    }}
-                    \\begin{{document}}
-                    \\pagestyle{{fancy}}
-                    \\fancyhead{{}} \\fancyfoot{{}}
-                    \\fancyhead[L]{{\\setfont {now().strftime("%Y-%m-%d %H:%M:%S")}}}
-                    \\fancyhead[C]{{\\setfont {self.username}}}
-                    \\fancyhead[R]{{\\setfont \\href{{https://github.com/biostat821/ehr-utils-project-status/tree/v1.0.0}}{{ehr-utils-project-status 1.0.0}}}}
-                    \\ttfamily
-                    \\fontseries{{l}}\\selectfont
-                    \\small""").strip()
-        document += "\n\\pagebreak\n".join(pages)
-        document += "\n\\end{document}"
-        document = document.replace("_", "\\_")
-        with open(f"outputs/{self.username}.tex", "w") as f:
-            f.write(document)
+        write_document(self.username, summaries)
 
     def parse_pr(self, pr):
         timeline_items = [edge["node"] for edge in pr["timelineItems"]["edges"]]
